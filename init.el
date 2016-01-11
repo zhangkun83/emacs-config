cb300cd50b162af2f66e6d50932f5715369ec1f9
Kun Zhang <zhangkun@google.com>
2016-01-11 11:16:02 -0800

    Load google-specific bits in init-google.el

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 
+;;;; Load lua-mode
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+;;;; Load google-specific bits
+(load-file (expand-file-name "~/.emacs.d/init-google.el"))
