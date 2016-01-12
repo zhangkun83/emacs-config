32e2e0ecc4325d4eb05d011873511591fc2ec196
Kun Zhang <zhangkun@google.com>
2016-01-11 23:35:53 -0800

    Add etags-select

--- init.el
+++ init.el
@@ -1,5 +1,11 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 
+;;;; etags-select
+(load "etags-select.el")
+(global-set-key "\M-?" 'etags-select-find-tag-at-point)
+(global-set-key "\M-." 'etags-select-find-tag)
+
+;;;; Always do case-sensitive search
 (setq-default tags-case-fold-search nil)
 (setq-default case-fold-search nil)
 
@@ -8,8 +14,16 @@
 ;;;; Enable Semantic (semantic doesn't work if Java file contains generics)
 ; (semantic-mode 1)
 
+;;;; The ido mode for more interactive searching
 (require 'ido)
 (ido-mode t)
+
+
+;;;; Java outline regexp
+(add-hook 'java-mode-hook
+  (lambda ()
+    "Enable Java outline."
+    (setq outline-regexp "\\(?:\\([ \t]*.*\\(class\\|interface\\)[ \t]+[a-zA-Z0-9_]+[ \t\n]*\\({\\|extends\\|implements\\)\\)\\|[ \t]*\\(public\\|private\\|static\\|final\\|native\\|synchronized\\|transient\\|volatile\\|strictfp\\| \\|\t\\)*[ \t]+\\(\\([a-zA-Z0-9_]\\|\\( *\t*< *\t*\\)\\|\\( *\t*> *\t*\\)\\|\\( *\t*, *\t*\\)\\|\\( *\t*\\[ *\t*\\)\\|\\(]\\)\\)+\\)[ \t]+[a-zA-Z0-9_]+[ \t]*(\\(.*\\))[ \t]*\\(throws[ \t]+\\([a-zA-Z0-9_, \t\n]*\\)\\)?[ \t\n]*{\\)")))
 
 ;;;; Load lua-mode
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
@@ -18,6 +32,8 @@
 
 ;;;; Load google-specific bits
 (load-file (expand-file-name "~/.emacs.d/init-google.el"))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
