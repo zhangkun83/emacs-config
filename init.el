a717353057ce8d5d23266f66188a0bd2d9820c05
Kun Zhang <zhangkun@google.com>
2016-01-14 13:28:12 -0800

    Switch to terminus font and a dark-background theme.
    I am not a fan of dark-background color schemes, but most of the bitmap
    fonts are so thin that work terribly with light backgrounds. The
    previously used font (misc-fixed) is not that thin on large sizes, but
    that was too large.

--- init.el
+++ init.el
@@ -6,7 +6,8 @@
 
 
 ;;;; text font
-(set-default-font "-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-1")
+;(set-default-font "-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-1")
+(set-default-font "-xos4-terminus-medium-r-normal-*-17-*-*-*-*-*-iso8859-1")
 
 ;;;; magit (git integration)
 (require 'magit)
@@ -88,6 +89,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (misterioso)))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -95,3 +101,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(set-cursor-color "#ffffff")
