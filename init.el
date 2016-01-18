0b8b8ab28df7736d31a615c4ce5756ea7a3f045e
Kun Zhang <zhangkun@google.com>
2016-01-18 14:30:36 -0800

    Change ace-jump keys from "C-c SPC" to "C-C C-SPC"
    C-c SPC is already used by org-mode.

--- init.el
+++ init.el
@@ -56,9 +56,9 @@
 
 ;; ace-jump-mode for faster cursor movement
 (autoload 'ace-jump-mode "ace-jump-mode" "Emacs quick move minor mode" t)
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-c C-SPC") 'ace-jump-mode)
 (autoload 'ace-jump-mode-pop-mark "ace-jump-mode" "Ace jump back:-)" t)
-(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+(define-key global-map (kbd "C-x C-SPC") 'ace-jump-mode-pop-mark)
 
 
 ;;; Load markdown-mode
