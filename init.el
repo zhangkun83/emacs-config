bcad7d35e0027563d2de8074bfab034dd2465baf
Kun Zhang <zhangkun@google.com>
2016-01-18 20:56:19 -0800

    Enable org-indent-mode automatically.

--- init.el
+++ init.el
@@ -124,6 +124,7 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes (quote (misterioso)))
+ '(org-startup-indented t)
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
