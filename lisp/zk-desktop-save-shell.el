0aa8326733e11cc91b513a3d3b1626b6bded80a7
Kun Zhang <zhangkun@google.com>
2016-01-29 09:39:42 -0800

    Nicer restoration of shell buffers.
    Make bash run "cat" at starting, instead of tampering the buffer
    input. This way the prompt of current session won't mess with the
    restored content.

--- lisp/zk-desktop-save-shell.el
+++ lisp/zk-desktop-save-shell.el
@@ -13,12 +13,15 @@
 (defun zk-shell-restore-desktop-buffer (file-name buffer-name directory)
   "The buffer-restoring handler for shell buffers"
   (setq default-directory directory)
-  (let ((buffer (shell buffer-name)))
+  (let ((saved-bash-args explicit-bash-args)
+	(bash-autorun-command (concat "cat '" (zk-get-shell-save-dir desktop-dirname) buffer-name "'; echo")))
     (progn
-      (erase-buffer)
-      (insert (concat "cat '" (zk-get-shell-save-dir desktop-dirname) buffer-name "'"))
-      (comint-send-input)
-      buffer))) ; I don't know how to flawlessly restore the shell session.
+      (setq explicit-bash-args ; run bash -c "cat '<FILE>'; echo; exec bash --noediting -i"
+	    (list "-c" (concat bash-autorun-command "; exec bash --noediting -i")))
+      (let ((buffer (shell buffer-name)))
+	(erase-buffer)
+	(setq explicit-bash-args saved-bash-args)
+	buffer)))) ; I don't know how to flawlessly restore the shell session.
 	        ; Inserting the text directly to the buffer would make the whole content
 	        ; treated as command input, which is a mess.
 	        ; Here I just print out the previous content, which is good enough.
