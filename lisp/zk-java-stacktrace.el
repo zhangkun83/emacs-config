50f9e718ec08a666ca275ce95383d543e83877a0
Kun Zhang <zhangkun@google.com>
2016-01-31 18:18:07 -0800

    The stack trace now really works.
    Fixed issues from previous comment:
    
    1. Interactive user input is not supposed to be used in the middle of a
    function. It will cause quirky behavior. Use predefined regexp to deal
    with duplicate files.
    
    2. This feature would conflict with original stack trace detection. Do
    not load my implementation by default. User needs to enable it explicitly.

--- lisp/zk-java-stacktrace.el
+++ lisp/zk-java-stacktrace.el
@@ -1,5 +1,4 @@
 ;;; Detection of Java stack traces in compilation-mode and compilation-minor-mode
-;;; TODO should also support .groovy
 (defun zk-java-stacktrace-regexp-to-filename ()
   "Generates a relative filename from zk-java-stacktrace-regexp regexp match data."
   (let ((relative-path
@@ -7,20 +6,46 @@
                  (match-string 2))))
     (zk-java-stacktrace-find-path relative-path)))
 
+(require 'dash)
+
+(defcustom zk-java-stacktrace-path-regex "src/"
+  "*The regex for selecting the actual file paths for Java stack traces."
+  :group 'zk
+  :type 'regexp)
+
 (defun zk-java-stacktrace-find-path (relative-path)
   "Given a relative path of a Java source file, find a matching absolute path
 and let user choose one if there are multiple matches"
   (let ((candidates
-         ; e.g., bash -c 'find . | grep \'io/grpc/Metadata.java$\''
-         (process-lines "bash" "-c" (concat "find . | grep '" relative-path "$'"))))
-    (if (> (length candidates) 1)
-        (completing-read "Select file: " candidates)
-      candidates)))
+	 ;; Must use string-match-p instead of string-match here, because the latter would change
+	 ;; the current state which conflicts with the work of compilation-error-regexp-alist-alist.
+	 (-filter (lambda (path) (string-match-p zk-java-stacktrace-path-regex path))
+		  ;; e.g., bash -c 'find . | grep \'io/grpc/Metadata.java$\'; echo -n'
+		  ;; The "echo -n" is to erase the return code 1 of grep when there is no match.
+		  ;; Emacs is not happy with non-zero return codes.
+		  (process-lines "bash" "-c" (concat "find . | grep '" relative-path "$' ; echo -n")))))
+    (progn
+      (if (> (length candidates) 1)
+	  (warn "More than one candidates for %s: %s . The first one will be chosen.
+ Please set zk-java-stacktrace-path-regex to prevent such case."
+		relative-path candidates))
+      (car candidates))))
 
-(add-to-list 'compilation-error-regexp-alist 'zk-java-stacktrace-regexp)
-(add-to-list 'compilation-error-regexp-alist-alist
-  '(zk-java-stacktrace-regexp .
-    ("^[[:space:]]*at \\(\\(?:[[:lower:]]+\\.\\)+\\)[^(]+(\\([[:alnum:]]+\\.java\\):\\([[:digit:]]+\\))"
-     zk-java-stacktrace-regexp-to-filename 3)))
+(add-to-list 'compilation-error-regexp-alist 'java)
+
+(defun zk-java-stacektrace-enable ()
+  "Enable zk's Java stack trace detection for compilation mode.
+ This may override existing such functionality from other sources."
+  (interactive)
+  ;; This regex does not only match .java, but all file extensions in similar pattern, e.g., .groovy
+  ;; The 'java entry was there initially, we are replacing it.
+  (setq compilation-error-regexp-alist-alist
+	(assq-delete-all 'java compilation-error-regexp-alist-alist))
+  (add-to-list
+   'compilation-error-regexp-alist-alist
+   '(java .
+	  ("^[[:space:]]*at \\(\\(?:[[:lower:]]+\\.\\)+\\)[^(]+(\\([[:alnum:]]+\\.[[:lower:]]+\\):\\([[:digit:]]+\\))"
+	   zk-java-stacktrace-regexp-to-filename 3)))
+  (message "Enabled zk's Java stacktrace detection"))
 
 (provide 'zk-java-stacktrace)
