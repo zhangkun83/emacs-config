365f74712c89632816fcaad6ee62bb49c40ba268
Kun Zhang <zhangkun@google.com>
2016-01-31 22:47:22 -0800

    zk-find-src-file-in-project.
    Finds files out of SRCFILES index.

--- lisp/zk.el
+++ lisp/zk.el
@@ -10,4 +10,11 @@
 
 (zk-set-project-root zk-project-root)
 
+(defun zk-find-src-file-in-project(f)
+  "Find a src file indexed in SRCFILES of this project."
+  (interactive
+   (list (completing-read "Find a src file: "
+                          (process-lines "bash" "-c" (concat "cat '" (expand-file-name zk-project-root) "/SRCFILES'; echo -n")))))
+  (find-file f))
+
 (provide 'zk)
