c9fc266e5a8d77cf4ba35b78d760d65d3e6b395c
Kun Zhang <zhangkun@google.com>
2016-12-21 13:15:16 -0800

    Easier-to-press alternative mappings

--- init.el
+++ init.el
@@ -176,12 +176,15 @@
 	  (lambda()
 	    "Register my own shortcuts for Java mode"
 	    (local-set-key (kbd "C-c i") 'zk-insert-java-import)
-            (local-set-key (kbd "C-M-y") 'zk-java-enter-braces-block)
+            (local-set-key (kbd "C-M-j") 'zk-java-enter-braces-block)
             (local-set-key (kbd "M-n") 'zk-java-next-thing)
             (local-set-key (kbd "C-M-n") 'zk-java-next-thing)
             (local-set-key (kbd "M-p") 'zk-java-prev-thing)
             (local-set-key (kbd "C-M-p") 'zk-java-prev-thing)))
 (global-set-key (kbd "C-x M-f") 'zk-find-src-file-in-project)
+;; Which is easier to press than C-M-u given my current key mapping
+;; which put Meta and Control under left and right thumbs.
+(global-set-key (kbd "C-M-i") 'backward-up-list)
 
 ; Java stacktrace detection in compilation-mode
 (require 'zk-java-stacktrace)
