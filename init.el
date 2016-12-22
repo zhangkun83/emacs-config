1eb477d0c28b4598820152e085a122197cdc51a3
Kun Zhang <zhangkun@google.com>
2016-12-21 21:52:35 -0800

    Delete ace-jump.
    It's not very useful. isearch is a faster useful way to move around.

--- init.el
+++ init.el
@@ -85,13 +85,6 @@
       (all-completions "" obarray 'commandp))))))
 
 
-;; ace-jump-mode for faster cursor movement
-(autoload 'ace-jump-mode "ace-jump-mode" "Emacs quick move minor mode" t)
-(define-key global-map (kbd "C-c C-SPC") 'ace-jump-mode)
-(autoload 'ace-jump-mode-pop-mark "ace-jump-mode" "Ace jump back:-)" t)
-(define-key global-map (kbd "C-x C-SPC") 'ace-jump-mode-pop-mark)
-
-
 ;;; Load markdown-mode
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
